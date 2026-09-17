f5c821da03d6101bc63d8ab4ca77cd451f2ae1e6
Raja <raja.rathinam@pcisage.com>
2026-09-17 20:26:46 +0530

    header fix

--- src/components/layout/AOSProvider/AOSProvider.js
+++ src/components/layout/AOSProvider/AOSProvider.js
@@ -9,14 +9,14 @@ import { Image } from "react-bootstrap";
 import ExpandOnView from "@/components/common/ExpandOnView/ExpandOnView";
 
 export default function AOSProvider({ children }) {
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
   const [isHidden, setIsHidden] = useState(false);
 
   // Minimum preloader duration
   useEffect(() => {
     const timer = setTimeout(() => {
       setIsLoading(false);
-    }, 0);
+    }, 1000);
 
     return () => clearTimeout(timer);
   }, []);
